98d67297f809df6aa138e8669021fb09b1d3a373
Andrew Deryabin <a.deryabin@formaltechnology.com>
2019-04-14 08:51:37 +0400

    Revert "Fix for cooperative use od nanovg and MetalNanoVG."
    This reverts commit 42d6948c88f406fc953913726e548ae5f3e2d5ec.
    
    Nanovg module was pointing to a wrong commit number. That was cured.
        So current fix is not needed anymore.

--- src/nanovg_mtl.h
+++ src/nanovg_mtl.h
@@ -30,34 +30,6 @@ extern "C" {
 
 #include "nanovg.h"
 
-// Create flags
-
-#ifdef NANOVG_GL_H
-    #define NVG_DOUBLE_BUFFER ((NVGcreateFlags)(1 << 12))
-    #define NVG_TRIPLE_BUFFER ((NVGcreateFlags)(1 << 13))
-#else
-    enum NVGcreateFlags {
-        // Flag indicating if geometry based anti-aliasing is used (may not be
-        // needed when using MSAA).
-        NVG_ANTIALIAS = 1 << 0,
-        // Flag indicating if strokes should be drawn using stencil buffer.
-        // The rendering will be a little slower, but path overlaps
-        // (i.e. self-intersecting or sharp turns) will be drawn just once.
-        NVG_STENCIL_STROKES = 1 << 1,
-        // Flag indicating if double buffering scheme is used.
-        NVG_DOUBLE_BUFFER = 1 << 12,
-        // Flag indicating if triple buffering scheme is used.
-        NVG_TRIPLE_BUFFER = 1 << 13,
-        // Flag indicating that additional debug checks are done.
-        NVG_DEBUG = 1 << 2,
-    };
-
-    // These are additional flags on top of NVGimageFlags.
-    enum NVGimageFlagsMetal {
-        NVG_IMAGE_NODELETE = 1 << 16, // Do not delete Metal texture handle.
-    };
-#endif
-
 struct MNVGframebuffer {
   NVGcontext* ctx;
   int image;
